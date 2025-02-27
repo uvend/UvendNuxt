2bf3c0c73245a1aecb69b3e184d3490818707e53
jaredleechrstians <jaredleechristians@gmail.com>
2025-02-27 15:17:16 +0200

    fix rollback payment card style

---
+++ components/my/CustomerCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <Card class="w-[300px] p-4">
+        <div>
+            {{ customer.description }}
+        </div>
+    </Card>
+</template>
+<script>
+export default{
+    props:{
+        customer: Object
+    }
+}
+</script>
