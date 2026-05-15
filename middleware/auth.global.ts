2e98984964de6f307724bc8e50b88d76871265eb
Jared <jaredleechristians@gmail.com>
2026-05-15 20:58:08 +0200

    fix: layout error on noauth redirect

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -6,16 +6,21 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     //console.log('middleware', APP_ENV)
     const path = to.path.split('/');
+    const redirectToLogin = () => {
+        setPageLayout('noauth')
+        return navigateTo('/login', { replace: true })
+    }
+
     if (to.meta.layout !== 'noauth') {
         let redirect = false;
         const auth = localStorage.getItem('token')
         const walletAuth = localStorage.getItem('wallet-access-token');
         if (!auth && APP_ENV == '') {
             console.error('You are not logged in to app')
-            return navigateTo('/login')
+            return redirectToLogin()
         }else if(!walletAuth && APP_ENV == 'wallet'){
             console.error('You are not logged in to wallet')
-            return navigateTo('/login')
+            return redirectToLogin()
         }
     }
 
